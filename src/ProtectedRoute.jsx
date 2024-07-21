7c6e1dc2c7326fca66cb4e221d86e4b0c2ba85e4
sergiod <sergiod@dos.sergio>
2024-07-21 12:48:53 -0600

    refactor: add checkLogin

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -1,8 +1,13 @@
 import { Navigate, Outlet } from "react-router-dom"
 import { useAuth } from "./context/AuthContext"
+import { useEffect } from "react"
 
 function ProtectedRoute() {
-  const { isAuthenticated, loading } = useAuth()
+  const { isAuthenticated, loading, checkLogin } = useAuth()
+
+  useEffect(()=>{
+    checkLogin()
+  },[])
 
   if (loading) return <h1>Loading...</h1>
   if (!loading && !isAuthenticated) return <Navigate to="/login" replace />
