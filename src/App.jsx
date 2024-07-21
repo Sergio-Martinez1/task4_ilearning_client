745e7f9949948ea235661c6965df3f38ef25e7c4
sergiod <sergiod@dos.sergio>
2024-07-21 13:40:48 -0600

    refactor: add home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/LoginPage.jsx'
 import SignupPage from './pages/SignupPage.jsx'
 import AdminPage from './pages/AminPage.jsx'
+import Home from './pages/HomePage.jsx'
 import ProtectedRoute from './ProtectedRoute.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
 import NavBar from './components/NavBar.jsx'
@@ -13,6 +14,7 @@ function App() {
         <div className='w-full h-screen relative flex flex-col'>
           <NavBar />
           <Routes>
+            <Route path='/' element={<Home />} />
             <Route path='/login' element={<LoginPage />} />
             <Route path='/signup' element={<SignupPage />} />
             <Route element={<ProtectedRoute />}>
